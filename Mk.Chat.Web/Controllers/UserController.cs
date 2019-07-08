1bbdeb8097ef58bfac983a79a56b7d1e8450d938
Makich Karagezyan <mak.kar@mail.ru>
2019-07-08 12:07:22 +0300

    add UserController

---
+++ Mk.Chat.Web/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mk.Chat.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET
+        [HttpGet]
+        [Route("api/users")]
+        public IActionResult GetUsers()
+        {
+
+
+            return Json(new[] {new {Id = 1, Name = "Vova"}, new {Id = 2, Name = "Dima"}});
+        }
+    }
+}
